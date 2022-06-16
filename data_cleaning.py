e952cccff8c887731f2329fb3c1f928094ef1eaa
Wayne Rose <e.guishardrose@hotmail.co.uk>
2022-06-16 19:11:18 +0100

    fixed a typo from data_cleaning script. Dataframes should now upload to RDS without error.

--- data_cleaning.py
+++ data_cleaning.py
@@ -34,7 +34,7 @@ class DataCleaning:
         raw_data = pd.read_json(file_pathway, encoding='utf-8-sig')
         return raw_data
     
-    def clean_dataframe(self, file_pathway):
+    def clean_dataframe(self, file_pathway, table_name):
         # Create the dataframe 
         raw_data = self.create_dataframe(file_pathway)
 
@@ -48,7 +48,7 @@ class DataCleaning:
         raw_data = raw_data.astype(
             {
             'title': 'string', 
-            'tink_to_page': 'string', 
+            'link_to_page': 'string', 
             'platform': 'string',
             'release_date': 'string',
             'developer': 'string',  
@@ -69,11 +69,11 @@ class DataCleaning:
         # Lastly, for each column in the description column, strip the word 'Summary:' off of each of the records. 
         raw_data.description = raw_data.description.str.strip('Summary:')
         raw_data.head()
-        raw_data.to_sql("Fighting_Games", engine=self.engine, if_exists='replace')
+        raw_data.to_sql(table_name, con=self.engine, if_exists='replace')
         # return the raw_data as a cleaned dataframe
         return raw_data
 
 if __name__ == "__main__":
     file_path = os.getcwd()
     new_data = DataCleaning()
-    new_data.clean_dataframe(file_path + '\\json-files\\fighting-games-details.json')
+    new_data.clean_dataframe(file_path + '\\json-files\\fighting-games-details.json', 'Fighting_Games')
